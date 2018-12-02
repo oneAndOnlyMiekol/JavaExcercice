dd3964d5484f6e00c6fe39014d5574ab90afef77
miekol <miekol@miekol-Lenovo>
2018-12-02 09:20:11 +0100

    2018-12-02

---
+++ E1_VariablesAndDataTypes/Excercice1_1/app/Variables.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Variables {
+	
+	public static void main(String[] args) {
+		System.out.printf("%.2f + %.2f + %.2f = %.2f",4.5,4.234,7.4355,(4.5+4.234+7.4355));
+	}
+
+}
---
+++ E1_VariablesAndDataTypes/Excercice1_2/app/Variables1.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Variables1 {
+
+	public static void main(String[] args) {
+		double a=4.5,b=44.327,c=23333,sum=a+b+c;
+		System.out.printf("%.2f + %.2f + %.2f = %.2f",a,b,c,sum);
+
+	}
+
+}
